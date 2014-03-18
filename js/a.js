bff50505bce5bae5e95363be5c077016db283347
Justin Lee <lee.justin.h@gmail.com>
2014-03-18 02:25:04 -0400

    Files added

---
+++ js/a.js
@@ -0,0 +1,1007 @@
+var a = [
+  {
+    "word":"fuck",
+    "agr":-0.120624,
+    "p":1.35E-217
+  },
+  {
+    "word":"fucking",
+    "agr":-0.113133,
+    "p":4.25E-194
+  },
+  {
+    "word":"shit",
+    "agr":-0.110044,
+    "p":5.20E-186
+  },
+  {
+    "word":"bitch",
+    "agr":-0.103248,
+    "p":5.81E-167
+  },
+  {
+    "word":"damn",
+    "agr":-0.085421,
+    "p":8.02E-113
+  },
+  {
+    "word":"hell",
+    "agr":-0.083178,
+    "p":8.10E-107
+  },
+  {
+    "word":"bitches",
+    "agr":-0.082082,
+    "p":2.53E-104
+  },
+  {
+    "word":"ass",
+    "agr":-0.080282,
+    "p":5.89E-100
+  },
+  {
+    "word":"fucked",
+    "agr":-0.080148,
+    "p":5.92E-098
+  },
+  {
+    "word":"wtf",
+    "agr":-0.077224,
+    "p":7.67E-092
+  },
+  {
+    "word":"fuck you",
+    "agr":-0.075006,
+    "p":9.81E-086
+  },
+  {
+    "word":"fuckin",
+    "agr":-0.074821,
+    "p":3.19E-085
+  },
+  {
+    "word":"bullshit",
+    "agr":-0.074243,
+    "p":2.55E-085
+  },
+  {
+    "word":"a bitch",
+    "agr":-0.071405,
+    "p":8.15E-079
+  },
+  {
+    "word":"this shit",
+    "agr":-0.069956,
+    "p":3.41E-075
+  },
+  {
+    "word":"pissed",
+    "agr":-0.069776,
+    "p":1.07E-074
+  },
+  {
+    "word":"fucked up",
+    "agr":-0.069533,
+    "p":1.00E-073
+  },
+  {
+    "word":"piss",
+    "agr":-0.062877,
+    "p":1.90E-060
+  },
+  {
+    "word":"fuck off",
+    "agr":-0.062009,
+    "p":1.47E-058
+  },
+  {
+    "word":"kill",
+    "agr":-0.061556,
+    "p":4.30E-057
+  },
+  {
+    "word":"what the fuck",
+    "agr":-0.060842,
+    "p":5.02E-056
+  },
+  {
+    "word":"shitty",
+    "agr":-0.058565,
+    "p":4.89E-052
+  },
+  {
+    "word":"the hell",
+    "agr":-0.058522,
+    "p":5.32E-052
+  },
+  {
+    "word":"so fucking",
+    "agr":-0.05835,
+    "p":1.72E-051
+  },
+  {
+    "word":"fuck up",
+    "agr":-0.057825,
+    "p":1.47E-050
+  },
+  {
+    "word":"drunk",
+    "agr":-0.056818,
+    "p":6.31E-049
+  },
+  {
+    "word":"give a fuck",
+    "agr":-0.052026,
+    "p":1.61E-040
+  },
+  {
+    "word":"bastard",
+    "agr":-0.051304,
+    "p":3.41E-039
+  },
+  {
+    "word":"fucks",
+    "agr":-0.050882,
+    "p":1.25E-038
+  },
+  {
+    "word":"dick",
+    "agr":-0.0507,
+    "p":5.63E-038
+  },
+  {
+    "word":"f",
+    "agr":-0.050502,
+    "p":3.87E-038
+  },
+  {
+    "word":"fake",
+    "agr":-0.049961,
+    "p":4.47E-037
+  },
+  {
+    "word":"whore",
+    "agr":-0.049118,
+    "p":8.98E-036
+  },
+  {
+    "word":"pissed off",
+    "agr":-0.049027,
+    "p":1.04E-035
+  },
+  {
+    "word":"fucking hate",
+    "agr":-0.048567,
+    "p":6.32E-035
+  },
+  {
+    "word":"asshole",
+    "agr":-0.048128,
+    "p":2.75E-034
+  },
+  {
+    "word":"niggas",
+    "agr":-0.047695,
+    "p":1.41E-033
+  },
+  {
+    "word":"the fuck up",
+    "agr":-0.047661,
+    "p":1.42E-033
+  },
+  {
+    "word":"stupid",
+    "agr":-0.047569,
+    "p":2.09E-032
+  },
+  {
+    "word":"sex",
+    "agr":-0.047404,
+    "p":3.06E-033
+  },
+  {
+    "word":"as hell",
+    "agr":-0.047166,
+    "p":6.97E-033
+  },
+  {
+    "word":"dead",
+    "agr":-0.047057,
+    "p":2.32E-032
+  },
+  {
+    "word":"my ass",
+    "agr":-0.0469,
+    "p":1.48E-032
+  },
+  {
+    "word":"i hate",
+    "agr":-0.046774,
+    "p":1.04E-030
+  },
+  {
+    "word":"cunt",
+    "agr":-0.046526,
+    "p":7.34E-032
+  },
+  {
+    "word":"fucker",
+    "agr":-0.045686,
+    "p":1.39E-030
+  },
+  {
+    "word":"hate",
+    "agr":-0.04557,
+    "p":3.48E-029
+  },
+  {
+    "word":"fuckers",
+    "agr":-0.045343,
+    "p":3.76E-030
+  },
+  {
+    "word":"i fucking hate",
+    "agr":-0.045108,
+    "p":8.70E-030
+  },
+  {
+    "word":"fuckin '",
+    "agr":-0.045018,
+    "p":1.20E-029
+  },
+  {
+    "word":"to kill",
+    "agr":-0.044813,
+    "p":2.65E-029
+  },
+  {
+    "word":"the fuck is",
+    "agr":-0.044645,
+    "p":5.00E-029
+  },
+  {
+    "word":"bloody",
+    "agr":-0.044484,
+    "p":5.08E-029
+  },
+  {
+    "word":"get the fuck",
+    "agr":-0.044122,
+    "p":1.94E-028
+  },
+  {
+    "word":"wtf ?",
+    "agr":-0.04389,
+    "p":3.78E-028
+  },
+  {
+    "word":"pussy",
+    "agr":-0.043575,
+    "p":1.46E-027
+  },
+  {
+    "word":"pathetic",
+    "agr":-0.0434,
+    "p":2.22E-027
+  },
+  {
+    "word":"the fuck out",
+    "agr":-0.043332,
+    "p":2.73E-027
+  },
+  {
+    "word":"hoes",
+    "agr":-0.043262,
+    "p":3.59E-027
+  },
+  {
+    "word":"nigga",
+    "agr":-0.042901,
+    "p":1.49E-026
+  },
+  {
+    "word":"punch",
+    "agr":-0.042768,
+    "p":1.59E-026
+  },
+  {
+    "word":"stfu",
+    "agr":-0.042285,
+    "p":6.92E-026
+  },
+  {
+    "word":"like shit",
+    "agr":-0.04212,
+    "p":1.30E-025
+  },
+  {
+    "word":"suck",
+    "agr":-0.041894,
+    "p":4.64E-025
+  },
+  {
+    "word":"die",
+    "agr":-0.041859,
+    "p":5.73E-025
+  },
+  {
+    "word":"fat",
+    "agr":-0.041711,
+    "p":3.54E-025
+  },
+  {
+    "word":"swear",
+    "agr":-0.041706,
+    "p":1.01E-024
+  },
+  {
+    "word":"i swear",
+    "agr":-0.041682,
+    "p":1.06E-024
+  },
+  {
+    "word":"don't give a",
+    "agr":-0.041265,
+    "p":1.44E-024
+  },
+  {
+    "word":"shut",
+    "agr":-0.041245,
+    "p":1.52E-024
+  },
+  {
+    "word":"murder",
+    "agr":-0.041123,
+    "p":2.26E-024
+  },
+  {
+    "word":"f *",
+    "agr":-0.041092,
+    "p":2.56E-024
+  },
+  {
+    "word":"knife",
+    "agr":-0.040875,
+    "p":5.54E-024
+  },
+  {
+    "word":"*",
+    "agr":-0.040701,
+    "p":1.57E-023
+  },
+  {
+    "word":"smoke",
+    "agr":-0.040652,
+    "p":1.06E-023
+  },
+  {
+    "word":"why the fuck",
+    "agr":-0.04065,
+    "p":1.21E-023
+  },
+  {
+    "word":"shut up",
+    "agr":-0.040596,
+    "p":1.52E-023
+  },
+  {
+    "word":"wtf is",
+    "agr":-0.040485,
+    "p":1.69E-023
+  },
+  {
+    "word":"motherfucker",
+    "agr":-0.040394,
+    "p":2.57E-023
+  },
+  {
+    "word":"cum",
+    "agr":-0.040384,
+    "p":2.21E-023
+  },
+  {
+    "word":"goddamn",
+    "agr":-0.040358,
+    "p":3.07E-023
+  },
+  {
+    "word":"burn",
+    "agr":-0.039965,
+    "p":8.89E-023
+  },
+  {
+    "word":"aint",
+    "agr":-0.039904,
+    "p":1.56E-022
+  },
+  {
+    "word":"sick of",
+    "agr":-0.039711,
+    "p":2.56E-022
+  },
+  {
+    "word":"death",
+    "agr":-0.039681,
+    "p":2.38E-022
+  },
+  {
+    "word":"assholes",
+    "agr":-0.039563,
+    "p":2.30E-022
+  },
+  {
+    "word":"lmao",
+    "agr":-0.03952,
+    "p":3.70E-022
+  },
+  {
+    "word":"shut the fuck",
+    "agr":-0.039294,
+    "p":5.84E-022
+  },
+  {
+    "word":"bastards",
+    "agr":-0.038863,
+    "p":2.17E-021
+  },
+  {
+    "word":"porn",
+    "agr":-0.038853,
+    "p":1.91E-021
+  },
+  {
+    "word":"haters",
+    "agr":-0.038852,
+    "p":1.90E-021
+  },
+  {
+    "word":"rape",
+    "agr":-0.038748,
+    "p":3.28E-021
+  },
+  {
+    "word":"to piss",
+    "agr":-0.037807,
+    "p":3.61E-020
+  },
+  {
+    "word":"idiots",
+    "agr":-0.03776,
+    "p":3.98E-020
+  },
+  {
+    "word":"a bitch .",
+    "agr":-0.037664,
+    "p":5.44E-020
+  },
+  {
+    "word":"piss me",
+    "agr":-0.037654,
+    "p":5.72E-020
+  },
+  {
+    "word":"drink",
+    "agr":-0.037602,
+    "p":6.72E-020
+  },
+  {
+    "word":"damned",
+    "agr":-0.037535,
+    "p":8.06E-020
+  },
+  {
+    "word":"who the fuck",
+    "agr":-0.037395,
+    "p":1.26E-019
+  },
+  {
+    "word":"retarded",
+    "agr":-0.03738,
+    "p":2.03E-019
+  },
+  {
+    "word":"",
+    "agr":null,
+    "p":null
+  },
+  {
+    "word":"birthday wishes !",
+    "agr":0.036052,
+    "p":1.17E-017
+  },
+  {
+    "word":"at church",
+    "agr":0.036221,
+    "p":2.86E-018
+  },
+  {
+    "word":"joy",
+    "agr":0.036244,
+    "p":4.74E-018
+  },
+  {
+    "word":"grateful",
+    "agr":0.036311,
+    "p":1.21E-017
+  },
+  {
+    "word":"hope everyone",
+    "agr":0.036494,
+    "p":5.50E-018
+  },
+  {
+    "word":"so thankful",
+    "agr":0.03655,
+    "p":2.73E-018
+  },
+  {
+    "word":"had an amazing",
+    "agr":0.036551,
+    "p":2.10E-018
+  },
+  {
+    "word":"beautiful day !",
+    "agr":0.036661,
+    "p":2.36E-018
+  },
+  {
+    "word":"happy",
+    "agr":0.036828,
+    "p":1.50E-018
+  },
+  {
+    "word":"much fun",
+    "agr":0.036835,
+    "p":2.13E-018
+  },
+  {
+    "word":"of christ",
+    "agr":0.03684,
+    "p":5.91E-019
+  },
+  {
+    "word":"so excited !",
+    "agr":0.036854,
+    "p":2.01E-018
+  },
+  {
+    "word":"an amazing day",
+    "agr":0.036882,
+    "p":6.41E-019
+  },
+  {
+    "word":"isaiah",
+    "agr":0.036972,
+    "p":3.68E-019
+  },
+  {
+    "word":"a great weekend",
+    "agr":0.037397,
+    "p":3.92E-019
+  },
+  {
+    "word":"faith",
+    "agr":0.037415,
+    "p":1.25E-019
+  },
+  {
+    "word":"thank",
+    "agr":0.037443,
+    "p":6.65E-019
+  },
+  {
+    "word":"loving",
+    "agr":0.037457,
+    "p":3.75E-019
+  },
+  {
+    "word":"of the lord",
+    "agr":0.037467,
+    "p":1.08E-019
+  },
+  {
+    "word":"family",
+    "agr":0.037646,
+    "p":1.11E-018
+  },
+  {
+    "word":"friends and family",
+    "agr":0.038119,
+    "p":3.27E-019
+  },
+  {
+    "word":"spring",
+    "agr":0.03818,
+    "p":2.00E-020
+  },
+  {
+    "word":"! can't wait",
+    "agr":0.038205,
+    "p":2.91E-020
+  },
+  {
+    "word":"tomorrow",
+    "agr":0.038442,
+    "p":9.64E-021
+  },
+  {
+    "word":"a blast",
+    "agr":0.038615,
+    "p":6.42E-021
+  },
+  {
+    "word":"day today",
+    "agr":0.038672,
+    "p":4.14E-021
+  },
+  {
+    "word":"praying for",
+    "agr":0.03893,
+    "p":3.33E-021
+  },
+  {
+    "word":"birthday wishes",
+    "agr":0.038949,
+    "p":2.00E-020
+  },
+  {
+    "word":"was awesome",
+    "agr":0.038959,
+    "p":1.81E-021
+  },
+  {
+    "word":"christmas",
+    "agr":0.039013,
+    "p":4.11E-021
+  },
+  {
+    "word":"beautiful",
+    "agr":0.039074,
+    "p":8.31E-021
+  },
+  {
+    "word":"the lord is",
+    "agr":0.03908,
+    "p":1.09E-021
+  },
+  {
+    "word":"a beautiful",
+    "agr":0.039221,
+    "p":1.00E-020
+  },
+  {
+    "word":"worship",
+    "agr":0.039669,
+    "p":1.90E-022
+  },
+  {
+    "word":"praying",
+    "agr":0.040041,
+    "p":1.81E-022
+  },
+  {
+    "word":"had an awesome",
+    "agr":0.040044,
+    "p":8.05E-023
+  },
+  {
+    "word":"a beautiful day",
+    "agr":0.040129,
+    "p":3.20E-022
+  },
+  {
+    "word":"much fun !",
+    "agr":0.040132,
+    "p":1.04E-022
+  },
+  {
+    "word":"the lord,",
+    "agr":0.040261,
+    "p":3.89E-023
+  },
+  {
+    "word":"awesome",
+    "agr":0.04033,
+    "p":3.82E-023
+  },
+  {
+    "word":"romans",
+    "agr":0.040361,
+    "p":2.19E-023
+  },
+  {
+    "word":"a wonderful day",
+    "agr":0.040367,
+    "p":1.15E-022
+  },
+  {
+    "word":"grace",
+    "agr":0.040492,
+    "p":1.91E-023
+  },
+  {
+    "word":"was awesome !",
+    "agr":0.040579,
+    "p":1.54E-023
+  },
+  {
+    "word":"amazing day",
+    "agr":0.040586,
+    "p":2.07E-023
+  },
+  {
+    "word":"a great time",
+    "agr":0.040654,
+    "p":3.06E-023
+  },
+  {
+    "word":"thank you lord",
+    "agr":0.040798,
+    "p":1.22E-023
+  },
+  {
+    "word":"please pray for",
+    "agr":0.040923,
+    "p":1.16E-023
+  },
+  {
+    "word":"happy thanksgiving",
+    "agr":0.04147,
+    "p":3.65E-024
+  },
+  {
+    "word":"great time",
+    "agr":0.04158,
+    "p":1.94E-024
+  },
+  {
+    "word":"love you",
+    "agr":0.042026,
+    "p":5.73E-024
+  },
+  {
+    "word":"great weekend",
+    "agr":0.042072,
+    "p":4.92E-025
+  },
+  {
+    "word":"god's",
+    "agr":0.042179,
+    "p":1.91E-025
+  },
+  {
+    "word":"so blessed",
+    "agr":0.042207,
+    "p":3.87E-025
+  },
+  {
+    "word":"wonderful day",
+    "agr":0.04224,
+    "p":4.65E-025
+  },
+  {
+    "word":"goodness",
+    "agr":0.042302,
+    "p":2.19E-025
+  },
+  {
+    "word":"please pray",
+    "agr":0.042341,
+    "p":1.94E-025
+  },
+  {
+    "word":"in the lord",
+    "agr":0.042495,
+    "p":3.69E-026
+  },
+  {
+    "word":"had a wonderful",
+    "agr":0.042606,
+    "p":2.42E-025
+  },
+  {
+    "word":":)",
+    "agr":0.042709,
+    "p":1.82E-024
+  },
+  {
+    "word":"pray",
+    "agr":0.043811,
+    "p":2.74E-027
+  },
+  {
+    "word":"beautiful day",
+    "agr":0.043987,
+    "p":6.55E-027
+  },
+  {
+    "word":"thankful for",
+    "agr":0.044063,
+    "p":8.51E-027
+  },
+  {
+    "word":"praise god",
+    "agr":0.04423,
+    "p":1.97E-028
+  },
+  {
+    "word":"prayer",
+    "agr":0.044274,
+    "p":8.11E-027
+  },
+  {
+    "word":"great day !",
+    "agr":0.044518,
+    "p":2.29E-028
+  },
+  {
+    "word":"blessings",
+    "agr":0.044847,
+    "p":6.16E-028
+  },
+  {
+    "word":"was amazing !",
+    "agr":0.044886,
+    "p":3.97E-029
+  },
+  {
+    "word":"corinthians",
+    "agr":0.045006,
+    "p":9.94E-030
+  },
+  {
+    "word":"great",
+    "agr":0.045009,
+    "p":2.37E-029
+  },
+  {
+    "word":"pray for",
+    "agr":0.04512,
+    "p":6.55E-029
+  },
+  {
+    "word":"you lord",
+    "agr":0.045141,
+    "p":1.42E-029
+  },
+  {
+    "word":"thankful",
+    "agr":0.045906,
+    "p":5.21E-029
+  },
+  {
+    "word":"god has",
+    "agr":0.046074,
+    "p":1.74E-030
+  },
+  {
+    "word":"praise",
+    "agr":0.046463,
+    "p":1.29E-031
+  },
+  {
+    "word":"church",
+    "agr":0.04652,
+    "p":7.16E-032
+  },
+  {
+    "word":"was amazing",
+    "agr":0.046911,
+    "p":8.96E-032
+  },
+  {
+    "word":"merry christmas",
+    "agr":0.047137,
+    "p":2.23E-032
+  },
+  {
+    "word":"proverbs",
+    "agr":0.047549,
+    "p":2.00E-033
+  },
+  {
+    "word":"a great day",
+    "agr":0.048621,
+    "p":4.49E-034
+  },
+  {
+    "word":"love you all",
+    "agr":0.048655,
+    "p":1.82E-034
+  },
+  {
+    "word":"so excited",
+    "agr":0.048739,
+    "p":6.07E-033
+  },
+  {
+    "word":"! thank you",
+    "agr":0.048788,
+    "p":2.19E-034
+  },
+  {
+    "word":"psalm",
+    "agr":0.049001,
+    "p":1.22E-035
+  },
+  {
+    "word":"thanksgiving",
+    "agr":0.04938,
+    "p":3.93E-035
+  },
+  {
+    "word":"in christ",
+    "agr":0.049739,
+    "p":8.04E-037
+  },
+  {
+    "word":"thank you",
+    "agr":0.050974,
+    "p":2.91E-037
+  },
+  {
+    "word":"excited for",
+    "agr":0.051146,
+    "p":1.90E-037
+  },
+  {
+    "word":"great day",
+    "agr":0.052462,
+    "p":2.18E-040
+  },
+  {
+    "word":"an amazing",
+    "agr":0.052477,
+    "p":7.23E-041
+  },
+  {
+    "word":"had a great",
+    "agr":0.052764,
+    "p":9.38E-041
+  },
+  {
+    "word":"the lord",
+    "agr":0.05295,
+    "p":8.34E-042
+  },
+  {
+    "word":"an awesome",
+    "agr":0.053257,
+    "p":1.41E-042
+  },
+  {
+    "word":"a wonderful",
+    "agr":0.054659,
+    "p":1.91E-041
+  },
+  {
+    "word":"prayers",
+    "agr":0.056504,
+    "p":3.91E-045
+  },
+  {
+    "word":"amazing",
+    "agr":0.056682,
+    "p":9.91E-048
+  },
+  {
+    "word":"wonderful",
+    "agr":0.056881,
+    "p":1.03E-044
+  },
+  {
+    "word":"a great",
+    "agr":0.056981,
+    "p":1.20E-047
+  },
+  {
+    "word":"blessed",
+    "agr":0.057403,
+    "p":1.01E-045
+  },
+  {
+    "word":"excited",
+    "agr":0.059002,
+    "p":8.55E-049
+  }
+]
---
+++ js/main.js
@@ -0,0 +1,96 @@
+/*
+* DATA FILES: 
+* o.js
+* c.js
+* e.js
+* a.js
+* n.js
+*/
+
+/*global $:false*/
+/*jshint indent:2*/
+
+'use strict';
+
+$( document ).ready( function() {
+
+  // Array of OCEAN values
+  var ocean    = [0.50, 0.50, 0.50, 0.50, 0.50];
+  var aLengths = [o.length, c.length, e.length, a.length, n.length];
+  var text     = $( '.generated' );
+  var generate = $( '#generate' );
+  var append   = $( '#append' );
+  var inputs   = $( '[type="range"]' );
+  var firstClick = true;
+  var count = 0;
+  var maxNGrams = 10;
+
+  // Update OCEAN array
+  inputs.change( function() {
+    switch (this.name) {
+      case 'open':
+        ocean[0] = this.value/100;
+        break;
+      case 'consc':
+        ocean[1] = this.value/100;
+        break;
+      case 'extra':
+        ocean[2] = this.value/100;
+        break;
+      case 'agree':
+        ocean[3] = this.value/100;
+        break;
+      case 'neuro':
+        ocean[4] = this.value/100;
+        break;
+    }
+  });
+
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function within (min, max, value) {
+    if (value > max) {
+      value = max;
+    }
+    if (value < min) {
+      value = min;
+    }
+    return value;
+  }
+
+
+  function generator() {
+    var r = 7*getRandomArbitrary(-1.0, 1.0);
+
+    var oi = within(0, aLengths[0]-1, Math.round(ocean[0]*aLengths[0]+r));
+    var ci = within(0, aLengths[1]-1, Math.round(ocean[1]*aLengths[1]+r));
+    var ei = within(0, aLengths[2]-1, Math.round(ocean[2]*aLengths[2]+r));
+    var ai = within(0, aLengths[3]-1, Math.round(ocean[3]*aLengths[3]+r));
+    var ni = within(0, aLengths[4]-1, Math.round(ocean[4]*aLengths[4]+r));
+    
+    text.append(o[oi].word + ' ');
+    text.append(c[ci].word + ' ');
+    text.append(e[ei].word + ' ');
+    text.append(a[ai].word + ' ');
+    text.append(n[ni].word + ' ');
+  }
+
+  // Call to generator
+  generate.click(function () {
+    count = 0;
+    text.empty();
+    generator();
+  });
+
+  // Add generated content 
+  append.click(function () {
+    if (firstClick) {
+      text.empty();
+      firstClick = false;
+    }
+    count++;
+    if (count < maxNGrams) generator();
+  });
+});
